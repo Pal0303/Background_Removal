71ed341f2ae18d8a8040338f41aba81e145e4d4e
Pal Dave <paldave1903@gmail.com>
2025-08-13 12:00:35 +0530

    User database commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+
+const PORT = process.env.PORT || 4000
+const app = express()
+await connectDB().then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch(err => {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
+});
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => res.send('API working'))
+app.listen(PORT, () => console.log('Server is running on port: '+PORT));
